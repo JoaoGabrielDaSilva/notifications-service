9cdbbb05baf149e990206f1f57ebd16c10142a3f
João <joao@3035tech.com>
2022-12-13 09:21:20 -0300

    (feat): notifications controller and validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { Body, Post } from '@nestjs/common/decorators'
+import { PrismaService } from './prisma.service'
+import { randomUUID } from 'node:crypto'
+import { CreateNotificationBody } from './create-notification-body'
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany()
+  }
+
+  @Post()
+  create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body
+
+    return this.prisma.notification.create({
+      data: {
+        recipientId,
+        content,
+        category,
+      },
+    })
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { Get } from '@nestjs/common/decorators'
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!'
   }
 }
